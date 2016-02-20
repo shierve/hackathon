dd22471d43e35c25c5367cf3d704876c2c38b487
lopezm94 <juanlopezm94@gmail.com>
2016-02-20 02:45:15 +0100

    App init

---
+++ android/src/com/mygdx/game/HomeScreenActivity.java
@@ -0,0 +1,16 @@
+package com.mygdx.game;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import butterknife.Bind;
+
+public class HomeScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+}
