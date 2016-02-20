1fada6114a2c3efc1dda609f8b37d6a8022c738d
lopezm94 <juanlopezm94@gmail.com>
2016-02-20 13:28:55 +0100

    Player next_position ready

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -5,6 +5,7 @@ package com.mygdx.game;
  */
 public class Player extends Car {
 
+    static private Direction next_dir;
     static private Player player = null;
 
     private Player(Street street, double position, double speed){
@@ -22,6 +23,78 @@ public class Player extends Car {
 
     @Override
     public void next_position(){
-
+        //TODO:check trafic lights
+        if (next_dir != null) {
+            Direction dir = getCarDirection();
+            switch (dir) {
+                case UP:
+                    switch(next_dir) {
+                        case UP:
+                            go_straight();
+                            break;
+                        case DOWN:
+                            u_turn();
+                            break;
+                        case RIGHT:
+                            turn_right();
+                            break;
+                        case LEFT:
+                            turn_left();
+                            break;
+                    }
+                    break;
+                case DOWN:
+                    switch(next_dir) {
+                        case UP:
+                            u_turn();
+                            break;
+                        case DOWN:
+                            go_straight();
+                            break;
+                        case RIGHT:
+                            turn_left();
+                            break;
+                        case LEFT:
+                            turn_right();
+                            break;
+                    }
+                    break;
+                case RIGHT:
+                    switch(next_dir) {
+                        case UP:
+                            turn_left();
+                            break;
+                        case DOWN:
+                            turn_right();
+                            break;
+                        case RIGHT:
+                            go_straight();
+                            break;
+                        case LEFT:
+                            u_turn();
+                            break;
+                    }
+                    break;
+                case LEFT:
+                    switch(next_dir) {
+                        case UP:
+                            turn_right();
+                            break;
+                        case DOWN:
+                            turn_left();
+                            break;
+                        case RIGHT:
+                            u_turn();
+                            break;
+                        case LEFT:
+                            go_straight();
+                            break;
+                    }
+                    break;
+            }
+        }
+        else
+            go_straight();
+        next_dir = null;
     }
 }
