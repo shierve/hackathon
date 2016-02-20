11a1c31232733ebee1e6f48a1d4e7c42d674e99d
lopezm94 <juanlopezm94@gmail.com>
2016-02-20 03:58:05 +0100

    HomeScreen Working

--- android/src/com/mygdx/game/HomeScreenActivity.java
+++ android/src/com/mygdx/game/HomeScreenActivity.java
@@ -1,16 +1,29 @@
 package com.mygdx.game;
 
+import android.app.LauncherActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class HomeScreenActivity extends Activity {
+public class HomeScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.start)
+    public void startListener(View view) {
+        Intent intent = new Intent(this, AndroidLauncher.class);
+        startActivity(intent);
+    }
 }
